769628fb902483d8a6c794e91529356c36defa95
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-05-28 19:29:37 -0400

    initial commit and Added Hero class into the driver class

---
+++ COMP123-S2016-Ckawakam-300821245-Assignment1/Hero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_Ckawakam_300821245_Assignment1
+{
+    /**
+     * This class defines a generic Hero
+     * 
+     * @class Hero
+     * @param 
+     * @field _strength{int},_speed{int},_health{int},_name{string},Name{string}
+     */
+    class Hero
+    {
+       
+    }
+}
---
+++ COMP123-S2016-Ckawakam-300821245-Assignment1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**
+ *This is driver class
+ *Author: Chinatsu Kawakami
+ *Date :May 28 th 2016
+ *Date Modified: 
+ *Student Id: 300821245
+ *Description:
+ *version : 0.0.1 - initial commit and Added Hero class into the driver class
+ */
+
+namespace COMP123_S2016_Ckawakam_300821245_Assignment1
+{
+    /**
+     * This is the driver class for our program , Assignment1
+     * 
+     * @class Program
+     */
+    class Program
+    {
+        /**
+         * The Main method for our driver class program
+         * 
+         * @constructor: Main
+         * @param {string[]}args
+         */
+        static void Main(string[] args)
+        {
+           
+
+
+        }
+    }
+}
